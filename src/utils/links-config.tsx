22d4f5c07c7ab280118acaee885a7c8c29449b05
Leonardo Braga <programer_frontbraga@outlook.com>
2024-11-20 10:35:49 -0300

    feat: created utils file [links-config]

---
+++ src/utils/links-config.tsx
@@ -0,0 +1,22 @@
+export const links = [
+	{
+		id: 'homePage',
+		name: 'Início',
+		href: '/',
+	},
+	{
+		id: 'aboutPage',
+		name: 'Sobre',
+		href: '/about',
+	},
+	{
+		id: 'projectsPage',
+		name: 'Projetos',
+		href: '/projects',
+	},
+	{
+		id: 'contactPage',
+		name: 'Contato',
+		href: '/contact',
+	},
+];
