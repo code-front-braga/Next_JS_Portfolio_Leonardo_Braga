798d38686f0eac6a6134b3cc8966fb74bc4531ab
Leonardo Braga <programer_frontbraga@outlook.com>
2024-11-20 11:10:17 -0300

    feat: created fonts file and defined typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,21 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {},
+			fontFamily: {
+				ubuntuCond: '"Ubuntu Condensed", sans-serif',
+				bebasNeue: '"Bebas Neue", cursive',
+				rajdhani: '"Rajdhani", sans-serif',
+				odibee: '"Odibee Sans", sans-seri',
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
