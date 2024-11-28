c814a32675b30f5ac946928d522d06a1fbb7703c
Leonardo Braga <programer_frontbraga@outlook.com>
2024-11-27 21:38:56 -0300

    fix: fixed path of the svg icons

--- src/lib/utils/icons.ts
+++ src/lib/utils/icons.ts
@@ -1,12 +1,12 @@
-import iconJS from './js_icon.svg';
-import iconHTML from './html_icon.svg';
-import iconCSS from './css_icon.svg';
-import iconTS from './ts_icon.svg';
-import iconSASS from './sass_icon.svg';
-import iconVSCODE from './vscode_icon.svg';
-import iconNEXTJS from './next_icon.svg';
-import iconGITHUB from './github_icon.svg';
-import iconGIT from './git_icon.svg';
+import iconJS from '../../../public/svg/js_icon.svg';
+import iconHTML from '../../../public/svg/html_icon.svg';
+import iconCSS from '../../../public/svg/css_icon.svg';
+import iconTS from '../../../public/svg/ts_icon.svg';
+import iconSASS from '../../../public/svg/sass_icon.svg';
+import iconVSCODE from '../../../public/svg/vscode_icon.svg';
+import iconNEXTJS from '../../../public/svg/next_icon.svg';
+import iconGITHUB from '../../../public/svg/github_icon.svg';
+import iconGIT from '../../../public/svg/git_icon.svg';
 
 export const icons = [
 	iconJS,
