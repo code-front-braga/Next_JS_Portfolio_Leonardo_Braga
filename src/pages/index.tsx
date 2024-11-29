de60bfb8e9c3016beb77c66d1a08df3ae1e87e13
Leonardo Braga <programer_frontbraga@outlook.com>
2024-11-28 22:04:01 -0300

    test: testing for iphone [9]

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,9 +55,8 @@ export default function Home() {
 				>
 					<div
 						className={clsx(
-							'relative h-[40%] w-[50%] transform-gpu overflow-hidden rounded-full bg-orangered outline outline-1 outline-offset-4 outline-orangered',
+							'relative h-[40%] w-[50%] overflow-hidden rounded-full bg-orangered outline outline-1 outline-offset-4 outline-orangered',
 							slider['translate-z'],
-
 							'mobile414px:h-[36%] mobile414px:w-[64%]',
 							'mobile480px:h-[42%]',
 							'mobile600px:w-[60%]',
